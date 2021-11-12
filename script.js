1dee4a68e4f0a5c933d2c6e023b1ca81f6026f22
emmalec <lecruitemmanuel@gmail.com>
2021-11-12 20:01:59 -0300

    deleteEmpleado en JQuery y Vanilla JS

--- script.js
+++ script.js
@@ -75,19 +75,18 @@ function getEmpleado() {
 }
 
 //document ready para asegurarnos que el DOM esta listo
-$(document).ready(function () {
-  $("#mostrarCards").on("click", () => {
-    let empleadosCard = getEmpleado();
+$("#mostrarCards").on("click", () => {
+  let empleadosCard = getEmpleado();
 
-    $("#globalContainer")
-      .empty() //para borrar el contenido de globalContainer
-      .append(`<div id="globalContainer_cards" class="row"></div>`);
+  $("#globalContainer")
+    .empty() //para borrar el contenido de globalContainer
+    .append(`<div id="globalContainer_cards" class="row"></div>`);
 
-    for (const empleado of empleadosCard) {
-      let edadEmpleado = empleado.calcularAnios(empleado.fechaNacimiento);
-      let antiguedadEmpleado = empleado.calcularAnios(empleado.fechaIngreso);
+  for (const empleado of empleadosCard) {
+    let edadEmpleado = empleado.calcularAnios(empleado.fechaNacimiento);
+    let antiguedadEmpleado = empleado.calcularAnios(empleado.fechaIngreso);
 
-      $("#globalContainer_cards").append(`<div class="col-lg-2 pb-3">
+    $("#globalContainer_cards").append(`<div class="col-lg-2 pb-3">
       <div class="shadow-sm card">
         <img src="https://media.istockphoto.com/vectors/missing-image-of-a-person-placeholder-vector-id1288129985?k=20&m=1288129985&s=612x612&w=0&h=OHfZHfKj0oqIDMl5f_oRqH13MHiB63nUmySYILbWbjE=" class="card-img-top" alt="...">
         <div class="card-body">
@@ -97,62 +96,82 @@ $(document).ready(function () {
         </div>
       </div>
     </div>`);
-    }
-  });
-
-  console.log("DOM ready!");
+  }
 });
 
 //Mostrar empleados JQuery sidebar y boton
 
-$("#mostrarEmpleados, #mostrarLista").on("click", function () {
-  $("#globalContainer")
-    .empty()
-    .append(
-      "<table id='tableSide' class='table table-hover'><tbody id='tb'></tbody></table>"
-    );
+$(document).ready(function () {
+  $("#mostrarEmpleados, #mostrarLista").on("click", function () {
+    $("#globalContainer")
+      .empty()
+      .append(
+        "<table id='tableSide' class='table table-hover'><tbody id='tb'></tbody></table>"
+      );
 
-  //armando el header de la table
-  $("#tableSide").prepend(`<thead><th>#id</th>
-  <th>Nombre</th>
-  <th>Apellido</th>
-  <th>Edad</th>
-  <th>Antiguedad</th>
-  <th>e-mail</th>
-  <th>Sueldo Basico</th>
-  <th>Delete</th></thead>`);
+    //armando el header de la table
+    $("#tableSide").prepend(`<thead><th>#id</th>
+      <th>Nombre</th>
+      <th>Apellido</th>
+      <th>Edad</th>
+      <th>Antiguedad</th>
+      <th>e-mail</th>
+      <th>Sueldo Basico</th>
+      <th>Delete</th></thead>`);
 
-  let empleadosTable = getEmpleado();
+    let empleadosTable = getEmpleado();
 
-  for (const empleado of empleadosTable) {
-    let edadEmpleado = empleado.calcularAnios(empleado.fechaNacimiento);
-    let antiguedadEmpleado = empleado.calcularAnios(empleado.fechaIngreso);
+    for (const empleado of empleadosTable) {
+      let edadEmpleado = empleado.calcularAnios(empleado.fechaNacimiento);
+      let antiguedadEmpleado = empleado.calcularAnios(empleado.fechaIngreso);
 
-    $("#tb").append(`<tr><td> ${empleado.id}</td>
-    <td>${empleado.nombre}</td>
-    <td>${empleado.apellido}</td>
-    <td>${edadEmpleado} años</td>
-    <td>${antiguedadEmpleado} años</td>
-    <td>${empleado.email}</td>
-    <td>${empleado.sueldoBasico}</td>
-    <td id="deleteEmpleado"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-trash-fill" viewBox="0 0 16 16">
-    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-    </svg></td></tr>`);
-  }
+      $("#tb").append(`<tr><td> ${empleado.id}</td>
+        <td>${empleado.nombre}</td>
+        <td>${empleado.apellido}</td>
+        <td>${edadEmpleado} años</td>
+        <td>${antiguedadEmpleado} años</td>
+        <td>${empleado.email}</td>
+        <td>${empleado.sueldoBasico}</td>
+        <td><button type="button" id="deleteEmpleado${empleado.id}" class="btn btn-danger deleteEmpleado">Delete<i class="far fa-trash-alt"></i></button>
+        </td></tr>`);
+    }
+  });
 });
 
-//Bootstrap toast para cuando cargamos un empleado - no me funciona con el refresh del submit
+//Delete empleado Jquery
 
-$(document).ready(function () {
-  let toastLiveExample = document.getElementById("liveToast");
-  if (formularioEmpleado) {
-    formularioEmpleado.addEventListener("submit", function () {
-      var toast = new bootstrap.Toast(toastLiveExample);
+function deleteEmpleado() {
+  let empleados = JSON.parse(localStorage.getItem("empleado"));
+  console.log("esto funciona!");
 
-      toast.show();
+  for (const empleado of empleados) {
+    $(`#deleteEmpleado${empleado.id}`).on("click", function () {
+      console.log("boton!");
     });
   }
+}
+
+//Delete empleado Vanilla
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  function deleteEmpleadoVan() {
+    let empleados = JSON.parse(localStorage.getItem("empleado"));
+
+    for (const empleado of empleados) {
+      let boton = document.getElementById(`deleteEmpleado${empleado.id}`);
+      console.log(empleado.id);
+      boton.addEventListener("click", eliminarEmp);
+
+      function eliminarEmp() {
+        console.log("aca estoy!");
+      }
+    }
+  }
+  console.log("DOM ready");
+  deleteEmpleadoVan();
 });
+
+deleteEmpleado();
 
 //Clear el localStorage
 
