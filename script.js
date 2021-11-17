25f32fc2e7a78d8ce3534296e0c137471fe338d6
emmalec <lecruitemmanuel@gmail.com>
2021-11-17 16:50:47 -0300

    agregando botones dentro de empleados

--- script.js
+++ script.js
@@ -104,6 +104,8 @@ $("#dashboard").on("click", () => {
     </div>`
     );
 
+  $("#buttons").empty();
+
   $("#globalContainer")
     .empty() //para borrar el contenido de globalContainer
     .append(`<div id="globalContainer_dash" class="row"></div>`);
@@ -138,40 +140,10 @@ $("#dashboard").on("click", () => {
   });
 });
 
-//document ready para asegurarnos que el DOM esta listo
-$("#mostrarCards").on("click", () => {
-  let empleadosCard = getEmpleado();
-
-  $("#globalContainer")
-    .empty() //para borrar el contenido de globalContainer
-    .append(`<div id="globalContainer_cards" class="row"></div>`);
-
-  for (const empleado of empleadosCard) {
-    let edadEmpleado = empleado.calcularAnios(empleado.fechaNacimiento);
-    let antiguedadEmpleado = empleado.calcularAnios(empleado.fechaIngreso);
-
-    $("#globalContainer_cards").append(`<div class="col-lg-2 pb-3 card-group">
-      <div class="card">
-        <img src="https://media.istockphoto.com/vectors/missing-image-of-a-person-placeholder-vector-id1288129985?k=20&m=1288129985&s=612x612&w=0&h=OHfZHfKj0oqIDMl5f_oRqH13MHiB63nUmySYILbWbjE=" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${empleado.nombre} ${empleado.apellido}</h5>
-          <p class="card-text mb-0"> Edad: ${edadEmpleado} años</p>
-          <p class="card-text mb-0"> Antiguedad: ${antiguedadEmpleado} años</p>
-        </div>
-      </div>
-    </div>`);
-  }
-
-  //CARD ANIMATION
-
-  $(".card").slideDown(200, function () {
-    $(this).slideUp(0).slideDown(300).toggleClass("shadow-sm");
-  });
-});
-
 //Mostrar empleados JQuery sidebar y boton
 
 $(document).ready(function () {
+  //Mostrar empleados
   $("#mostrarEmpleados, #mostrarLista").on("click", function () {
     $("#banner")
       .empty()
@@ -186,10 +158,16 @@ $(document).ready(function () {
     //banner animation
     $("#bannerDashboard").hide().slideDown(250); //agregar CSS;
 
+    $("#buttons").empty().prepend(`
+    <div>
+      <button id="mostrarLista" type="button" class="btn btn-primary">Mostrar lista</button>
+      <button id="mostrarCards" type="button" class="btn btn-primary">Mostrar cards</button>
+    </div>`);
+
     $("#globalContainer")
       .empty()
       .append(
-        "<table id='tableSide' class='table table-hover'><tbody id='tb'></tbody></table>"
+        `<table id='tableSide' class='table table-hover'><tbody id='tb'></tbody></table>`
       );
 
     //armando el header de la table
@@ -221,17 +199,50 @@ $(document).ready(function () {
       }
     }
 
+    //Mostrar cards
+
+    $("#mostrarCards").on("click", () => {
+      let empleadosCard = getEmpleado();
+
+      $("#globalContainer")
+        .empty() //para borrar el contenido de globalContainer
+        .append(`<div id="globalContainer_cards" class="row"></div>`);
+
+      for (const empleado of empleadosCard) {
+        let edadEmpleado = empleado.calcularAnios(empleado.fechaNacimiento);
+        let antiguedadEmpleado = empleado.calcularAnios(empleado.fechaIngreso);
+
+        $("#globalContainer_cards")
+          .append(`<div class="col-lg-2 pb-3 card-group">
+          <div class="card">
+            <img src="https://media.istockphoto.com/vectors/missing-image-of-a-person-placeholder-vector-id1288129985?k=20&m=1288129985&s=612x612&w=0&h=OHfZHfKj0oqIDMl5f_oRqH13MHiB63nUmySYILbWbjE=" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${empleado.nombre} ${empleado.apellido}</h5>
+              <p class="card-text mb-0"> Edad: ${edadEmpleado} años</p>
+              <p class="card-text mb-0"> Antiguedad: ${antiguedadEmpleado} años</p>
+            </div>
+          </div>
+        </div>`);
+      }
+
+      //CARD ANIMATION
+
+      $(".card").slideDown(200, function () {
+        $(this).slideUp(0).slideDown(300).toggleClass("shadow-sm");
+      });
+    });
+
     //Delete empleado Jquery
 
     function deleteEmpleado() {
       let empleados = JSON.parse(localStorage.getItem("empleado"));
-      console.log("esto funciona!");
 
       for (const empleado of empleados) {
         $(`#deleteEmpleado${empleado.id}`).on("click", function () {
-          console.log(`Empleado ${empleado.id}`);
+          //buscamos el index del array mediante el id de empleado
+          index = empleados.findIndex((x) => x.id === empleado.id);
           //eliminamos el value asicoado al id
-          empleados.splice(empleado, 1);
+          empleados.splice(index, 1);
           //update del localStorage
           localStorage.setItem("empleado", JSON.stringify(empleados));
           //para refresh
@@ -243,26 +254,6 @@ $(document).ready(function () {
     deleteEmpleado();
   });
 });
-
-//Delete empleado Vanilla
-
-/* document.addEventListener("DOMContentLoaded", function (event) {
-  function deleteEmpleadoVan() {
-    let empleados = JSON.parse(localStorage.getItem("empleado"));
-
-    for (const empleado of empleados) {
-      let boton = document.getElementById(`deleteEmpleado${empleado.id}`);
-      console.log(empleado.id);
-      boton.addEventListener("click", eliminarEmp);
-
-      function eliminarEmp() {
-        console.log("aca estoy!");
-      }
-    }
-  }
-  console.log("DOM ready");
-  deleteEmpleadoVan();
-}); */
 
 //Clear el localStorage
 
